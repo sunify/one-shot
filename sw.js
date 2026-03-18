a83686384386bc96ca9258cb86bb9c83db8f9e51
sunify <isuntc@gmail.com>
2026-03-18 17:26:45 +0000

    Deploying to gh-pages from @ sunify/one-shot@e785e16fb4e5283e0db62d17819c4f5c3cf48573 🚀

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'one-shot-v1'
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((names) =>
+      Promise.all(
+        names.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))
+      )
+    )
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const clone = response.clone()
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone))
+        return response
+      })
+      .catch(() => caches.match(event.request))
+  )
+})
